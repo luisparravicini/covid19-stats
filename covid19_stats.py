1698a0a533819a899b3892d38f8e8188a57a539f
Luis Parravicini <lparravi@gmail.com>
2020-03-21 17:39:49 -0300

    using again the subplot instead of plt

--- covid19_stats.py
+++ covid19_stats.py
@@ -12,7 +12,7 @@ df.rename(columns={'DateRep': 'Date'}, inplace=True)
 df = df.sort_values('Date', ascending=True)
 df.set_index('Date')
 
-# fig, ax = plt.subplot()
+fig, ax = plt.subplots()
 countries = ('AR', 'BR', 'CL', 'BO', 'PE', 'UY', 'PY', 'EC', 'CO', 'VE')
 for geoid in countries:
     geo_df = df[df['GeoId'] == geoid]
@@ -20,10 +20,11 @@ for geoid in countries:
     # geo_df['CumulativeDeaths'] = geo_df['Deaths'].cumsum().replace(0, np.nan)
     name = geo_df['Countries and territories'].iloc[0]
 
-    plt.plot(geo_df['Date'], geo_df['Cumulative'], label=name)
+    ax.plot(geo_df['Date'], geo_df['Cumulative'], label=name)
     # ax[1].plot(geo_df['Date'], geo_df['CumulativeDeaths'], label=name)
     # ax[1].set_title('Muertes')
 
+fig.autofmt_xdate()
 plt.legend()
 plt.xlabel('Fecha')
 plt.ylabel('Casos confirmados')
