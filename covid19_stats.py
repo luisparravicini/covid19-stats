3a1b760571e5abd9cd8ed7603fbaa7e75e550091
Luis Parravicini <lparravi@gmail.com>
2020-03-21 16:07:47 -0300

    testing plotting deaths

--- covid19_stats.py
+++ covid19_stats.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from pandas.plotting import register_matplotlib_converters
+
 register_matplotlib_converters()
 
 date = '2020-03-20'
@@ -11,15 +12,21 @@ df.rename(columns={'DateRep': 'Date'}, inplace=True)
 df = df.sort_values('Date', ascending=True)
 df.set_index('Date')
 
-for geoid in ('AR', 'BR', 'CL', 'BO', 'PE', 'UY', 'PY', 'EC', 'CO', 'VE'):
+# fig, ax = plt.subplot()
+countries = ('AR', 'BR', 'CL', 'BO', 'PE', 'UY', 'PY', 'EC', 'CO', 'VE')
+for geoid in countries:
     geo_df = df[df['GeoId'] == geoid]
     geo_df['Cumulative'] = geo_df['Cases'].cumsum().replace(0, np.nan)
+    # geo_df['CumulativeDeaths'] = geo_df['Deaths'].cumsum().replace(0, np.nan)
     name = geo_df['Countries and territories'].iloc[0]
+
     plt.plot(geo_df['Date'], geo_df['Cumulative'], label=name)
+    # ax[1].plot(geo_df['Date'], geo_df['CumulativeDeaths'], label=name)
+    # ax[1].set_title('Muertes')
 
 plt.legend()
 plt.xlabel('Fecha')
 plt.ylabel('Casos confirmados')
-plt.title(f'Casos confirmados por país al {date}')
+plt.title(f'Casos confirmados al {date}')
 
 plt.show()
