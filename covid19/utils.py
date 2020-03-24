5cf45594285e6d3af6c24b0fdb6ddd69905b3a07
Luis Parravicini <lparravi@gmail.com>
2020-03-24 18:12:21 -0300

    find_newest_dataset has optional argument to disable downloads

--- covid19/utils.py
+++ covid19/utils.py
@@ -7,30 +7,40 @@ from bs4 import BeautifulSoup
 import json
 
 
-def find_newest_dataset():
+def find_newest_dataset(download=True):
     date = datetime.today().strftime('%Y-%m-%d')
     fname_prefix = 'COVID-19-geographic-disbtribution-worldwide'
     fname = Path(f'{fname_prefix}-{date}.xlsx')
 
     if not fname.exists():
-        print(f'fetching {date} data')
-        BASE_URL = 'https://www.ecdc.europa.eu/sites/default/files/documents/'
-        res = requests.get(BASE_URL + str(fname))
-        if res.status_code == 200:
-            with open(fname, 'wb') as f:
-                f.write(res.content)
-        else:
-            data_files = sorted(Path('.').glob(fname_prefix + '*'))
-            if len(data_files) == 0:
-                print('no data files')
-                sys.exit(1)
-            fname = data_files[-1]
-            match = re.search(r'(\d{4}-\d{2}-\d{2})', str(fname))
-            if match is None:
-                print('couldn\'t extract date')
-                sys.exit(1)
-            date = match.group(1)
-            print(f'using {fname}')
+        if download:
+            print('looking for new data files')
+            DOWNLOAD_URL = 'https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide'
+            res = requests.get(DOWNLOAD_URL)
+            res.raise_for_status()
+            soup = BeautifulSoup(res.content, 'html.parser')
+            link = soup.select_one('.download a')
+            href = link.get('href')
+
+            print(f'fetching data file')
+            fname = Path(href).name
+            res = requests.get(href)
+            if res.status_code == 200:
+                with open(fname, 'wb') as f:
+                    f.write(res.content)
+
+        data_files = sorted(Path('.').glob(fname_prefix + '*'))
+        if len(data_files) == 0:
+            print('no data files')
+            sys.exit(1)
+        fname = data_files[-1]
+        match = re.search(r'(\d{4}-\d{2}-\d{2})', str(fname))
+        if match is None:
+            print('couldn\'t extract date')
+            sys.exit(1)
+        date = match.group(1)
+        print(f'using {fname}')
+
     return (fname, date)
 
 
