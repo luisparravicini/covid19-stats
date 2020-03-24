b1fae2c4365fb42ab7971353f37bd7de9321a896
Luis Parravicini <lparravi@gmail.com>
2020-03-24 07:26:33 -0300

    deaths per million inhabitants

--- covid19/growth_rate.py
+++ covid19/growth_rate.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from .utils import find_newest_dataset
+from .utils import find_newest_dataset, countries_population
 from pandas.plotting import register_matplotlib_converters
 
 register_matplotlib_converters()
 
 
+populations = countries_population()
 path, date = find_newest_dataset()
 df = pd.read_excel(path)
 df.rename(columns={'DateRep': 'Date'}, inplace=True)
@@ -14,21 +15,24 @@ df = df.sort_values('Date', ascending=True)
 df.set_index('Date')
 
 fig, ax = plt.subplots()
-countries = ('ES', 'IT', 'BE', 'NL', 'FR', 'DE', 'PT')
-#('AR', 'BR', 'CL', 'BO', 'PE', 'UY', 'PY', 'EC', 'CO', 'VE')
+#countries = ('BE', 'NL', 'DE', 'ES', 'IT')
+countries = ('AR', 'BR', 'CL', 'BO', 'PE', 'PY', 'EC', 'CO', 'VE')
 for geoid in countries:
     geo_df = df[df['GeoId'] == geoid]
-    geo_df['Cumulative'] = geo_df['Deaths'].cumsum()
-    geo_df['GrowthRate'] = geo_df['Cumulative'].pct_change()
-    name = geo_df['Countries and territories'].iloc[0]
+    name = geo_df['Countries and territories'].iloc[0].replace('_', ' ')
+    inhabitants = populations[name] / 1000000
+
+    geo_df['Cumulative'] = (geo_df['Deaths'] / inhabitants).cumsum()
+    # geo_df['GrowthRate'] = geo_df['Cumulative'].pct_change()
+    geo_df['GrowthRate'] = geo_df['Cumulative'].replace(0, np.nan)
 
     ax.plot(geo_df['Date'], geo_df['GrowthRate'], label=name)
 
-ax.axhline(y=1, linestyle='--')
+# ax.axhline(y=1, linestyle='--')
 
 fig.autofmt_xdate()
 plt.legend()
 plt.ylabel('Rate')
-plt.title('Growth rate')
+plt.title('Cumulative deaths per million inhabitantes')
 
 plt.show()
