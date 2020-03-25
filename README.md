c73d3096e4eead9346889b92eabc1d1dfd703b54
Luis Parravicini <lparravi@gmail.com>
2020-03-25 17:34:15 -0300

    shows how many days it takes to double the amount of deaths per country

--- README.md
+++ README.md
@@ -6,15 +6,21 @@ The script makes some graphs on covid19 cases using data from the [European Cent
 
 Each script creates a graphic:
 
+Confirmed cases:
+``python -m covid19.confirmed_cases``
+
+![plot 01](plot_01.png "Latam confirmed cases")
+
+How long did it take for the number of total confirmed deaths to double? (copied the idea from [ourworldindata](https://ourworldindata.org/coronavirus):
+``python -m covid19.doubles``
+
+![plot 01](plot_03.png "Double deaths in days")
+
 Growth rate of cases:
 ``python -m covid19.growth_rate``
 
 ![plot 02](plot_02.png "Growth rate")
 
-Confirmed cases:
-``python -m covid19.confirmed_cases``
-
-![plot 01](plot_01.png "Latam confirmed cases")
 
 
 **DISCLAIMER**: I'm no statistician, data scientist, epidemiologist, nor I have any knowledge on viruses. I just have some free time and I'm playing around with the dataset and matplotlib, the graphs might not be accurate and/or have errors.
---
+++ covid19/doubles.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+from .utils import find_newest_dataset
+from datetime import datetime
+from pandas.plotting import register_matplotlib_converters
+
+register_matplotlib_converters()
+
+
+path, date = find_newest_dataset(download=True)
+df = pd.read_excel(path)
+df.rename(columns={'DateRep': 'Date'}, inplace=True)
+df = df.sort_values('Date', ascending=True)
+df.set_index('Date')
+
+print('Doubles\tDeaths\tLocation')
+today = datetime.today()
+countries = df['GeoId'].dropna().unique()
+for geoid in countries:
+    geo_df = df[df['GeoId'] == geoid]
+    name = geo_df['Countries and territories'].iloc[0].replace('_', ' ')
+
+    geo_df['Cumulative'] = geo_df['Deaths'].cumsum()
+    max_deaths = geo_df['Cumulative'].iloc[-1]
+    half_current_max = max_deaths / 2
+
+    if half_current_max <= 0:
+        continue
+
+    row = geo_df[geo_df['Cumulative'] >= half_current_max].iloc[0]
+
+    doubles_in = (today - row['Date']).days
+    print("\t".join(map(str, (doubles_in, max_deaths, name))))
