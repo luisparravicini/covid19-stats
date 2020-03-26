7598f2ca38b2cbcf5d216dcc92a0623728198c00
Luis Parravicini <lparravi@gmail.com>
2020-03-26 20:02:46 -0300

    trying to make the same plot as https://ourworldindata.org/grapher/covid-confirmed-deaths-since-5th-death

--- covid19/growth_rate.py
+++ covid19/growth_rate.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from .utils import find_newest_dataset, countries_population, name_for
+from .utils import find_newest_dataset, name_for
 from pandas.plotting import register_matplotlib_converters
 
 register_matplotlib_converters()
 
 
-populations = countries_population()
 path, date = find_newest_dataset(download=True)
 df = pd.read_excel(path)
 df.rename(columns={'DateRep': 'Date'}, inplace=True)
@@ -15,24 +14,47 @@ df = df.sort_values('Date', ascending=True)
 df.set_index('Date')
 
 fig, ax = plt.subplots()
-#countries = ('BE', 'NL', 'DE', 'ES', 'IT')
-countries = ('AR', 'BR', 'CL', 'BO', 'PE', 'PY', 'EC', 'CO', 'VE')
+
+countries = ('CN', 'IT', 'ES', 'EC', 'BR', 'CL', 'EC', 'CO', 'US', 'MX')
+countries = ('CN', 'IT', 'AR', 'EC', 'MX', 'BR', 'US')
+max_days = 0
+min_deaths = 5
+max_deaths = 0
 for geoid in countries:
-    geo_df = df[df['GeoId'] == geoid]
-    name = name_for(geo_df)
-    inhabitants = populations[name] / 1000000
+    geo_df = df[df['GeoId'] == geoid].copy()
 
-    geo_df['Cumulative'] = (geo_df['Deaths'] / inhabitants).cumsum()
-    # geo_df['GrowthRate'] = geo_df['Cumulative'].pct_change()
-    geo_df['GrowthRate'] = geo_df['Cumulative'].replace(0, np.nan)
+    geo_df['Cumulative'] = geo_df['Deaths'].cumsum()
 
-    ax.plot(geo_df['Date'], geo_df['GrowthRate'], label=name)
+    geo_df.drop(index=geo_df[geo_df['Cumulative'] < min_deaths].index,
+                inplace=True)
+    geo_df['Cumulative'] -= min_deaths
 
-# ax.axhline(y=1, linestyle='--')
+    geo_df['DaysSince'] = range(len(geo_df))
 
-fig.autofmt_xdate()
+    ax.plot(geo_df['DaysSince'], geo_df['Cumulative'], label=name_for(geo_df))
+
+    max_days = max(max_days, len(geo_df))
+    max_deaths = max(max_deaths, geo_df['Cumulative'].iloc[-1])
+
+indexes = (2, 3, 5, 10)
+data = list()
+for i in range(max_days):
+    datum = [i] + list(map(lambda x: np.power(2, i / x), indexes))
+    data.append(datum)
+
+curves = pd.DataFrame(
+            data=data,
+            columns=['x'] + list(map(lambda x: f'd{x}', indexes))
+        )
+for d in indexes:
+    ax.plot(curves['x'], curves[f'd{d}'], linestyle='--', label=f'doubles in {d} days')
+
+
+ax.set_yscale('log')
 plt.legend()
-plt.ylabel('Rate')
-plt.title('Cumulative deaths per million inhabitantes')
+plt.ylim(top=max_deaths, bottom=1)
+plt.ylabel('Deaths')
+plt.xlabel(f'Days since first {min_deaths} deaths')
+plt.title('Cumulative deaths')
 
 plt.show()
